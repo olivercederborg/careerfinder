01ab8c1acaf9d2ab890da8014d98b9215a142695
Oliver Cederborg <3323050@rts-365.dk>
2021-04-27 22:03:52 +0200

    extend tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
 			boxShadow: {
 				yellow: "0 4px 12px 0 rgba(245, 158, 11, 50%)"
 			},
+			dropShadow: {
+				lightning: "0 2px 6px rgba(245, 158, 11, 50%)"
+			},
 			screens: {
 				"3xl": "2500px",
 				"4xl": "2800px"
