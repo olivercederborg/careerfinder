10068fcec2f1704c5307193da1e6b5211ebad879
Oliver Cederborg <3323050@rts-365.dk>
2021-04-30 00:30:12 +0200

    add second career image

--- careers.js
+++ careers.js
@@ -7,6 +7,8 @@ export const careers = [
 		hot: true,
 		image:
 			"https://images.unsplash.com/photo-1602992708529-c9fdb12905c9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
+		imageTwo:
+			"https://images.unsplash.com/photo-1573497161042-21fa63ecfebf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
 		description:
 			"Web developers design and create websites. They are responsible for the look of the site. They are also responsible for the site's technical aspects, such as its performance and capacity, which are measures of a website's speed and how much traffic the site can handle."
 	},
