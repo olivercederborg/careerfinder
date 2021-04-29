e23659ec06bf0dfac0a399ecb4f98514aedacbb5
Oliver Cederborg <hi@olivercederborg.com>
2021-04-29 16:35:24 +0200

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
 	mode: "jit",
 	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-	darkMode: false, // or 'media' or 'class'
+	darkMode: false,
 	theme: {
 		extend: {
+			colors: {
+				"gray-main": "#777777"
+			},
 			boxShadow: {
-				yellow: "0 4px 12px 0 rgba(245, 158, 11, 50%)"
+				yellow: "0 4px 12px 0 rgba(245, 158, 11, 50%)",
+				slight:
+					"0 8px 15px -3px rgba(0, 0, 0, 0.1), 0 2px 6px -2px rgba(0, 0, 0, 0.01)"
 			},
 			dropShadow: {
 				lightning: "0 2px 6px rgba(245, 158, 11, 50%)"
