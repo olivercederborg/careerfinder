e67f0b6c437c8ec1775051775d174b79d50bb0ca
Oliver Cederborg <3323050@rts-365.dk>
2021-05-01 00:42:15 +0200

    remove font links

--- pages/career.js
+++ pages/career.js
@@ -95,12 +95,6 @@ const Career = () => {
 		<>
 			<Head>
 				<title>{career.name} - CareerFinder</title>
-
-				<link rel='preconnect' href='https://fonts.gstatic.com' />
-				<link
-					href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap'
-					rel='stylesheet'
-				/>
 			</Head>
 
 			<Navbar />
