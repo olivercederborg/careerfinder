a1a8953fee90991d9b6665645fe798cdb8809a9f
Oliver Cederborg <3323050@rts-365.dk>
2021-05-01 00:41:35 +0200

    add viewport meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
+import Head from "next/head";
 import "tailwindcss/tailwind.css";
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>CareerFinder</title>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1, maximum-scale=1'
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
