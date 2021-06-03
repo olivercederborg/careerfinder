190188c931d232a5af53f42f62cef5daac046d57
Oliver Cederborg <3323050@rts-365.dk>
2021-06-03 23:02:01 +0200

    feat(courses-page): filters shell

--- components/CourseFiltersShell.js
+++ components/CourseFiltersShell.js
@@ -1,7 +1,7 @@
 export default function CourseFiltersShell({ children }) {
   return (
     <>
-      <section className="rounded-2xl container flex flex-wrap justify-between p-6 shadow-lg">
+      <section className="rounded-2xl gap-y-8 gap-x-10 container flex flex-wrap justify-between p-6 shadow-lg">
         {children}
       </section>
     </>
