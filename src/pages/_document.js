be3b43dd7dbbddf811eb6dbbdd4261389c6536ed
Casper Engelmann <me@casperengelmann.com>
2021-07-09 00:33:39 +0200

    I'm too old for this shit!

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,28 +1,33 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return { ...initialProps };
-	}
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
 
-	render() {
-		return (
-			<Html>
-				<Head>
-					<link rel='preconnect' href='https://fonts.gstatic.com' />
-					<link
-						href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap'
-						rel='stylesheet'
-					/>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <script
+            src="https://cdn.usefathom.com/script.js"
+            data-site="LIGUYOKZ"
+            defer
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
-export default MyDocument;
+export default MyDocument
