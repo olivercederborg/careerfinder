ff5f6922652a9f47c9f6ebe3f655e74240372888
Oliver Cederborg <hi@olivercederborg.com>
2021-10-24 20:44:13 +0200

    feat: add seo fields + hero fields to course category document

--- cms/schemas/CourseCategory.ts
+++ cms/schemas/CourseCategory.ts
@@ -1,8 +1,23 @@
-import { NAME_FIELD, slugField } from './primitives'
+import { NAME_FIELD, SEO_DESCRIPTION, SEO_TITLE, slugField } from './primitives'
 
 export const CourseCategory = {
   title: 'Course Category',
   name: 'courseCategory',
   type: 'document',
-  fields: [NAME_FIELD, slugField('name')],
+  fields: [
+    NAME_FIELD,
+    slugField('name'),
+    {
+      title: 'Hero Title',
+      name: 'heroTitle',
+      type: 'string',
+    },
+    {
+      title: 'Hero Subtitle',
+      name: 'heroSubtitle',
+      type: 'string',
+    },
+    SEO_TITLE,
+    SEO_DESCRIPTION,
+  ],
 }
