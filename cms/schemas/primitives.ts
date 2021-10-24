758fd2457423b5a611717df6d7a0c93380b35e82
Oliver Cederborg <hi@olivercederborg.com>
2021-10-24 20:43:29 +0200

    feat: add seo fields

--- cms/schemas/primitives.ts
+++ cms/schemas/primitives.ts
@@ -12,3 +12,23 @@ export const slugField = (source: string) => ({
     maxLength: 64,
   },
 })
+
+export const SEO_TITLE = {
+  name: 'seoTitle',
+  title: 'SEO Title',
+  type: 'string',
+  validation: (Rule) =>
+    Rule.max(60).warning(
+      `It is recommended to keep a title below 60 characters.`
+    ),
+}
+
+export const SEO_DESCRIPTION = {
+  name: 'seoDescription',
+  title: 'SEO Description',
+  type: 'text',
+  validation: (Rule) =>
+    Rule.max(160).warning(
+      `It is recommended to keep a description below 160 characters.`
+    ),
+}
