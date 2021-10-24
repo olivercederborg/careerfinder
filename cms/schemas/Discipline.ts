f24a7475f3eb33731eef1e483cfa2ae6afab3c63
Oliver Cederborg <hi@olivercederborg.com>
2021-10-24 21:16:15 +0200

    feat: add field descriptions

--- cms/schemas/Discipline.ts
+++ cms/schemas/Discipline.ts
@@ -11,14 +11,26 @@ export const Discipline = {
       title: 'Hero Title',
       name: 'heroTitle',
       type: 'string',
+      description:
+        'Hero Title for course category page tied to this discipline.',
     },
     {
       title: 'Hero Subtitle',
       name: 'heroSubtitle',
       type: 'string',
+      description:
+        'Hero Subtitle for course category page tied to this discipline.',
     },
-    SEO_TITLE,
-    SEO_DESCRIPTION,
+    {
+      description:
+        'SEO Title for course category page tied to this discipline.',
+      ...SEO_TITLE,
+    },
+    {
+      description:
+        'SEO Description for course category page tied to this discipline.',
+      ...SEO_DESCRIPTION,
+    },
   ],
 }
 
