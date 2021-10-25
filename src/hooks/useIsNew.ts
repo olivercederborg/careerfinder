cfdf078af1d72e86c8d5944cac5f7ba37000eae6
Oliver Cederborg <hi@olivercederborg.com>
2021-10-25 14:29:24 +0200

    feat: add jsdoc

--- src/hooks/useIsNew.ts
+++ src/hooks/useIsNew.ts
@@ -1,5 +1,11 @@
 import { differenceInDays } from 'date-fns'
 
+/**
+ * Determine if a date is within the specified threshold in days.
+ * @param isoDate - `string`
+ * @param daysToBeNew - `number`
+ * @returns `boolean`
+ */
 export function useIsNew(isoDate: string, daysToBeNew: number) {
   const difference = differenceInDays(new Date(), new Date(isoDate))
   return difference <= daysToBeNew
