4d37886e800118280e39c286224d4117526c0904
Oliver Cederborg <hi@olivercederborg.com>
2021-10-25 10:39:32 +0200

    feat: move role fields to job

--- cms/schemas/Job.ts
+++ cms/schemas/Job.ts
@@ -1,6 +1,5 @@
 import { ReferenceDiscipline } from './Discipline'
 import { ReferenceArea } from './Area'
-import { ReferenceRole } from './Role'
 import { NAME_FIELD, SEO_DESCRIPTION, SEO_TITLE, slugField } from './primitives'
 
 export const Job = {
@@ -10,9 +9,34 @@ export const Job = {
   fields: [
     NAME_FIELD,
     slugField('name'),
+    {
+      name: 'salary',
+      title: 'Salary',
+      type: 'number',
+    },
+    {
+      name: 'currency',
+      title: 'Currency',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'US Dollars', value: 'USD' },
+          { title: 'Euro', value: 'EUR' },
+          { title: 'Pound Sterling', value: 'GBP' },
+          { title: 'Canadian Dollars', value: 'CAD' },
+          { title: 'Danish Kroner', value: 'DKK' },
+          { title: 'Swedish Kroner', value: 'SEK' },
+          { title: 'Norwegian Kroner', value: 'NOK' },
+        ],
+      },
+    },
+    {
+      name: 'time',
+      title: 'Time',
+      type: 'string',
+    },
     ReferenceDiscipline,
     ReferenceArea,
-    ReferenceRole,
     {
       name: 'banner',
       title: 'Banner',
