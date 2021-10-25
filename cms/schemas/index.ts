271c478a44c7e99878393836aa647c258de97907
Oliver Cederborg <hi@olivercederborg.com>
2021-10-25 11:07:21 +0200

    add expect ts error for sanity imports

--- cms/schemas/index.ts
+++ cms/schemas/index.ts
@@ -1,6 +1,8 @@
 // First, we must import the schema creator
+// @ts-expect-error
 import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
+// @ts-expect-error
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
